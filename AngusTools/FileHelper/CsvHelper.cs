712a04adb4878f09052c6d9f9c8bed8d2f2cb22c
AngusLiuYi <154724136@qq.com>
2025-11-02 11:43:37 +0800

    首次提交
    首次提交

---
+++ AngusTools/FileHelper/CsvHelper.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngusTools.FileHelper
+{
+    public static class CsvHelper
+    {
+        #region List读写
+        /// <summary>
+        /// 读取csv文件，以List格式返回
+        /// 默认无表头返回
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        /// <returns></returns>
+        public static List<string> CsvToList(string path) => CsvToList(path, false);
+
+        /// <summary>
+        /// 读取csv文件，以List格式返回
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        /// <param name="isHasTitle">是否含有表头</param>
+        /// <returns></returns>
+        public static List<string> CsvToList(string path, bool isHasTitle)
+        {
+            if (!File.Exists(path))
+                return new List<string>();
+            return File.ReadAllLines(path).ToList();
+        }
+        #endregion
+
+        #region 读取到DataTable
+        /// <summary>
+        /// 读取csv文件，以Dt格式返回
+        /// 默认有表头
+        /// 默认编码方式GB2312
+        /// </summary>
+        /// <param name="path">文件地址</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DataTable CsvToDataTable(string path)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return CsvToDataTable(path, true, Encoding.GetEncoding("gb2312"));
+        }
+
+        /// <summary>
+        /// 读取csv文件，以Dt格式返回
+        /// 默认编码方式GB2312
+        /// </summary>
+        /// <param name="path">文件地址</param>
+        /// <param name="isHasTitle">是否含有表头</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DataTable CsvToDataTable(string path, bool isHasTitle)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return CsvToDataTable(path, isHasTitle, Encoding.GetEncoding("gb2312"));
+        }
+
+        /// <summary>
+        /// 读取csv文件，以Dt格式返回
+        /// 默认有表头
+        /// </summary>
+        /// <param name="path">文件地址</param>
+        /// <param name="encoding">文件编码方式（记事本通常为UTF-8，表格通常为GB2312）</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DataTable CsvToDataTable(string path, Encoding encoding) => CsvToDataTable(path, true, encoding);
+
+        /// <summary>
+        /// 读取csv文件，以Dt格式返回
+        /// </summary>
+        /// <param name="path">文件地址</param>
+        /// <param name="isHasTitle">是否含有表头</param>
+        /// <param name="encoding">文件编码方式（记事本通常为UTF-8，表格通常为GB2312）</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DataTable CsvToDataTable(string path, bool isHasTitle,Encoding encoding)
+        {
+            if (!File.Exists(path))
+                return new DataTable();
+            var lines = File.ReadAllLines(path, encoding).ToList();
+            bool isFirst = true;
+            DataTable dt = new DataTable();
+            foreach (var line in lines)
+            {
+                string[] str = line.Split(',');
+                if (isFirst)
+                {
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        dt.Columns.Add();
+                        if (isHasTitle)
+                            dt.Columns[i].ColumnName = str[i];
+                    }
+                }
+                if (str.Length == dt.Columns.Count)
+                {
+                    if (!isFirst)
+                        dt.Rows.Add(str);
+                }
+                else throw new Exception("csv文件错误：表格行列数量不一致");
+                isFirst = false;
+            }
+            return dt;
+        }
+        #endregion
+
+        #region 从DataTable写入到csv文件
+        /// <summary>
+        /// 将DT的数据写入到csv文件中保存
+        /// 默认有表头
+        /// 默认覆盖原文件
+        /// 默认编码GB2312
+        /// </summary>
+        /// <param name="dt">文件源</param>
+        /// <param name="path">目标文件地址</param>
+        /// <returns></returns>
+        public static bool DataTableToCsv(DataTable dt, string path)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return DataTableToCsv(dt, path, true, false, Encoding.GetEncoding("gb2312"));
+        }
+
+        /// <summary>
+        /// 将DT的数据写入到csv文件中保存
+        /// 默认覆盖原文件
+        /// 默认编码GB2312
+        /// </summary>
+        /// <param name="dt">文件源</param>
+        /// <param name="path">目标文件地址</param>
+        /// <param name="isHasTitle">是否写入表头</param>
+        /// <returns></returns>
+        public static bool DataTableToCsv(DataTable dt, string path, bool isHasTitle)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return DataTableToCsv(dt, path, isHasTitle, false, Encoding.GetEncoding("gb2312"));
+        }
+
+        /// <summary>
+        /// 将DT的数据写入到csv文件中保存
+        /// 默认编码GB2312
+        /// </summary>
+        /// <param name="dt">文件源</param>
+        /// <param name="path">目标文件地址</param>
+        /// <param name="isHasTitle">是否写入表头</param>
+        /// <param name="isAppend">追加或覆盖原文件</param>
+        /// <returns></returns>
+        public static bool DataTableToCsv(DataTable dt, string path, bool isHasTitle, bool isAppend)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            return DataTableToCsv(dt, path, isHasTitle, isAppend, Encoding.GetEncoding("gb2312"));
+        }
+
+        /// <summary>
+        /// 将DT的数据写入到csv文件中保存
+        /// 默认有表头
+        /// 默认覆盖原文件
+        /// </summary>
+        /// <param name="dt">文件源</param>
+        /// <param name="path">目标文件地址</param>
+        /// <param name="encoding">编码方式</param>
+        /// <returns></returns>
+        public static bool DataTableToCsv(DataTable dt, string path, Encoding encoding) => DataTableToCsv(dt, path, true, false, encoding);
+
+        /// <summary>
+        /// 将DT的数据写入到csv文件中保存
+        /// 默认覆盖原文件
+        /// </summary>
+        /// <param name="dt">文件源</param>
+        /// <param name="path">目标文件地址</param>
+        /// <param name="isHasTitle">是否写入表头</param>
+        /// <param name="encoding">编码方式</param>
+        /// <returns></returns>
+        public static bool DataTableToCsv(DataTable dt, string path, bool isHasTitle, Encoding encoding) => DataTableToCsv(dt, path, isHasTitle, false, encoding);
+
+
+        /// <summary>
+        /// 将DT的数据写入到csv文件中保存
+        /// </summary>
+        /// <param name="dt">文件源</param>
+        /// <param name="path">目标文件地址</param>
+        /// <param name="isHasTitle">是否写入表头</param>
+        /// <param name="isAppend">追加或覆盖原文件</param>
+        /// <param name="encoding">编码方式</param>
+        /// <returns></returns>
+        public static bool DataTableToCsv(DataTable dt, string path, bool isHasTitle, bool isAppend, Encoding encoding)
+        {
+            if (!File.Exists(path))
+                File.Create(path);
+            using (StreamWriter sw = new StreamWriter(path, isAppend, encoding))
+            {
+                if (isHasTitle)
+                {
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        sw.Write(dt.Columns[i].ColumnName);
+                        if (i != dt.Columns.Count - 1)
+                            sw.Write(",");
+                    }
+                    sw.WriteLine();
+                }
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        sw.Write(dt.Rows[i][j].ToString());
+                        if (j != dt.Columns.Count - 1)
+                            sw.Write(",");
+                    }
+                    sw.WriteLine();
+                }
+            }
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ AngusTools/LogHelper/LogDemo.cs
@@ -0,0 +1,57 @@
+﻿using AngusTools.LogHelper;
+using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngusTools.LogHelper
+{
+    public static class LogDemo
+    {
+        public static void TestShow()
+        {
+            LogManager.Info("记录一条Info等级的信息");
+            //正常写入日志
+            LogManager.Info("This is an info message: {0}", "TestInfo");
+            LogManager.Debug("This is a debug message: {0}", "TestDebug");
+            LogManager.Warn("This is an warning message: {0}", "TestInfo");
+            LogManager.Error("This is a error message: {0}", "TestDebug");
+
+            //自定义写入日志，一般情况下使用枚举定义日志记录器的名称
+            var logger = LogManager.GetLogger("测试日志");
+            logger.Info("This is an info message: {0}", "TestInfo");
+            logger.Debug("This is a debug message: {0}", "TestDebug");
+            logger.Warn("This is an warning message: {0}", "TestInfo");
+            logger.Error("This is a error message: {0}", "TestDebug");
+
+            // 在程序退出前关闭所有日志记录器，默认超时时间是3秒
+            //LogManager.Close(5);
+
+            //调试时偶尔使用
+            if (LogManager.LastException != null)
+                Console.WriteLine("日志异常:" + LogManager.LastException);
+
+            //配置方法(一般情况下使用默认配置即可)：
+            //自定义日志保存路径，默认保存到程序启动目录下的Log文件夹
+            LogManager.CustomLogPath = () => AppDomain.CurrentDomain.BaseDirectory + "\\CustomLogs";
+
+            //自定义日志文件名称，默认文件名为 DateTime.Now.ToString("yyyy-MM-dd") + ".log"
+            LogManager.CustomLogFileName = () => "MyLog_" + DateTime.Now.ToString("yyyyMMdd") + ".log";
+
+            //日志保存天数，默认30天
+            LogManager.SaveDays = 10;
+
+            //日志记录的格式，默认为 $"[{Time:yyyy-MM-dd HH:mm:ss ffff}] [{Level.ToString().ToUpper()}] [{ThreadId}] {Message}"
+            LogManager.LogFormatter = (item) =>
+            {
+                //可以在这里做日志等级筛选，如果返回string.Empty这该条日志不会记录到文件
+                return $"{item.Time:yyyy/MM/dd HH:mm:ss.fff} | {item.Level} | T{item.ThreadId:0000} | {item.Message}";
+            };
+
+            //日志回调，可用于界面实时显示日志或日志保存其它存储介质
+            LogManager.OnWriteLog = (item) => Console.WriteLine("An event was logged: " + item.ToString());
+        }
+    }
+}
