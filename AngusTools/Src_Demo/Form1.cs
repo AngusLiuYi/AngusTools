517314e7e37ed5cffa9b0dc570aa8a0cfc179055
AngusLiuYi <154724136@qq.com>
2025-11-02 15:20:02 +0800

    创建Test示例，测试cfg文件读写

--- AngusTools/Src_Demo/Form1.cs
+++ AngusTools/Src_Demo/Form1.cs
@@ -1,3 +1,4 @@
+using Src_Demo.FileHelper;
 using System.Data;
 
 namespace Src_Demo
@@ -11,10 +12,7 @@ namespace Src_Demo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string path = @"..\..\..\Data\Config.config";
-            string key = "ChangeType";
-            AngusTools.FileHelper.ConfigHelper.SaveValueToCfg(path, "ChangeType", "10");
-            int value = int.Parse(AngusTools.FileHelper.ConfigHelper.GetCfgValue(path, key));
+            Test_CfgHelper tc=new Test_CfgHelper();
         }
     }
 }
--- AngusTools/Src_Demo/obj/Debug/net8.0-windows/Src_Demo.AssemblyInfo.cs
+++ AngusTools/Src_Demo/obj/Debug/net8.0-windows/Src_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Src_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e417c963ae3481c16589f0f3234f9690b96447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f06f55d311ba759f9381ea5b5ddf6c231324c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Src_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Src_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
